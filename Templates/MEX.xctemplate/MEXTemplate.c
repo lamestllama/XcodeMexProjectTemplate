b5f46ec59067799788f3beaa7ee00d86e9856438
lamestllama <eric@eparsonage.com>
2015-03-29 23:08:31 +1030

    Initial commit

---
+++ Templates/MEX.xctemplate/MEXTemplate.c
@@ -0,0 +1,18 @@
+/*
+ *  ___PROJECTNAMEASIDENTIFIER___.c
+ *  ___PROJECTNAMEASIDENTIFIER___
+ *
+ *  Created by ___FULLUSERNAME___ on ___DATE___.
+ *  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+ *
+ */
+
+#include "___PROJECTNAMEASIDENTIFIER___.h"
+
+void mexFunction(int nlhs, 
+				 mxArray *plhs[], 
+				 int nrhs, 
+				 const mxArray *prhs[])
+{
+mexPrintf("Hello world!\n");
+};
---
+++ Templates/MEX.xctemplate/MEXTemplate.h
@@ -0,0 +1,15 @@
+/*
+ *  ___PROJECTNAMEASIDENTIFIER___.h
+ *  ___PROJECTNAMEASIDENTIFIER___
+ *
+ *  Created by ___FULLUSERNAME___ on ___DATE___.
+ *  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+ *
+ */
+
+#include "mex.h"
+
+void mexFunction(int nlhs, 
+				 mxArray *plhs[], 
+				 int nrhs, 
+				 const mxArray *prhs[]);
